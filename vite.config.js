3969b7f3750373457edb9a8b385b2d902ab12955
Zaid-M-M <zaid.m@parashifttech.com>
2024-03-30 12:11:01 +0530

    Your commit message here

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  define: {
+    'process.env.VITE_ID':JSON.stringify(process.env.VITE_ID)
+  }
 })
